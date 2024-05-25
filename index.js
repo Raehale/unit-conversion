ecea07cdf8c888707657ec8bdb0e78f717053679
Raehale <87107302+Raehale@users.noreply.github.com>
2024-05-24 18:04:52 -0600

    combining functions

--- index.js
+++ index.js
@@ -8,31 +8,23 @@ const convertBtn = document.getElementById("convert-btn");
 
 convertBtn.addEventListener("click", function() {
     const convertInput = document.getElementById("convert-input").value;
-    lengthConversion(convertInput);
-    volumeConversion(convertInput);
-    massConversion(convertInput);
+    convert(convertInput);
 });
 
-/***CONVERT LENGTH */
-function lengthConversion(number) {
+/***CONVERT ***/
+function convert(number) {
     const lengthConvertedText = document.getElementById("length-conversion");
+    const volumeConvertedText = document.getElementById("volume-conversion");
+    const massConvertedText = document.getElementById("mass-conversion");
+
     const feet = (number * 3.28084).toFixed(3);
     const meters = (number / 3.28084).toFixed(3);
-    lengthConvertedText.textContent = `${number} meters = ${feet} feet | ${number} feet = ${meters} meters`;
-}
-
-/***CONVERT VOLUME */
-function volumeConversion(number) {
-    const volumeConvertedText = document.getElementById("volume-conversion");
     const gallons = (number * 0.264).toFixed(3);
     const liters = (number / 0.264).toFixed(3);
-    volumeConvertedText.textContent = `${number} liters = ${gallons} gallons | ${number} gallons = ${liters} liters`;
-}
-
-/***CONVERT MASS */
-function massConversion(number) {
-    const massConvertedText = document.getElementById("mass-conversion");
     const pounds = (number * 2.204).toFixed(3);
     const kilos = (number / 2.204).toFixed(3);
+    
+    lengthConvertedText.textContent = `${number} meters = ${feet} feet | ${number} feet = ${meters} meters`;
+    volumeConvertedText.textContent = `${number} liters = ${gallons} gallons | ${number} gallons = ${liters} liters`;
     massConvertedText.textContent = `${number} kilos = ${pounds} pounds | ${number} pounds = ${kilos} kilos`;
 }
